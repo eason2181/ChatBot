169c90efaea468fd337bd54aceb18f4b030fba32
eason <eason2181@gmail.com>
2020-04-25 11:22:10 -0400

    First commit

---
+++ src/graphnode.cpp
@@ -0,0 +1,78 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <iostream>
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot;
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    //task4 change 2
+    
+    _childEdges.emplace_back(std::move( std::unique_ptr<GraphEdge>(edge) ));
+    edge = nullptr;
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    //_chatBot = chatbot;//shallow copy causing crush
+    //_chatBot->SetCurrentNode(this);
+}
+void GraphNode::MoveChatbotHere(std::unique_ptr<ChatBot> chatbot)
+{
+    _chatBot = std::move(chatbot);
+    _chatBot->SetCurrentNode(this);
+}
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    std::cout << chatbot.getChatLogic() << std::endl;
+    _ChatBotInst = std::move(chatbot);
+    _ChatBotInst.SetCurrentNode(this);
+}
+
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    //newNode->MoveChatbotHere(std::move(_chatBot));
+    newNode->MoveChatbotHere(std::move(_ChatBotInst));
+    //_chatBot = nullptr; // invalidate pointer at source
+    
+}
+
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
